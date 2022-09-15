389612add1b89423013f7e9a48047b1b1a4baf12
RicccardoRojas <ricccardo15@gmail.com>
2022-09-15 09:25:37 -0500

    Creacion de proyecto en c#

---
+++ PresentacionTienda/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PresentacionTienda
+{
+    public class Program
+    {
+        [STAThread]
+
+        public static void Main()
+        {
+        }
+    }
+}
