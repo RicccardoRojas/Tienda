957af8122299674a9ed89a23d6cccdf72b34a575
RicccardoRojas <ricccardo15@gmail.com>
2022-09-16 19:30:59 -0500

    Se agrego la conexion de datos

---
+++ AccesoDatosTienda/AccesoDatosProductos.cs
@@ -0,0 +1,26 @@
+﻿using EntidadesTienda;
+using ConectarBD;
+using System.Data;
+
+namespace AccesoDatosTienda
+{
+    public class AccesoDatosProductos
+    {
+        Base b = new Base("localhost", "root", "", "tiendagit");
+
+        public void Borrar(dynamic Entidad)
+        {
+            b.Comando(string.Format("call eliminarproducto({0})", Entidad.IDProductos));
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            b.Comando(string.Format("call insertarproducto('{0}','{1}',{2})", Entidad.Nombre, Entidad.Descripcion, Entidad.Precio));
+        }
+
+        public DataSet Mostrar(string Filtro)
+        {
+            return b.Obtener(string.Format("call showproducto('%{0}%')", Filtro), "Producto");
+        }
+    }
+}
---
+++ EntidadesTienda/Productos.cs
@@ -0,0 +1,18 @@
+﻿namespace EntidadesTienda
+{
+    public class Productos
+    {
+        public Productos(int iDProducto, string nombre, string descripcion, double precio)
+        {
+            IDProducto = iDProducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public int IDProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
